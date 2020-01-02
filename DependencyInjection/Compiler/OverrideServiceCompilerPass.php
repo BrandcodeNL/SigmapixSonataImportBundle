a35b08d6729a765912292026bf54c6085cf25ba2
brandon <brandon.delaparra@aveq.nl>
2020-01-02 10:26:19 +0100

    removed export service override, this is broken

--- DependencyInjection/Compiler/OverrideServiceCompilerPass.php
+++ DependencyInjection/Compiler/OverrideServiceCompilerPass.php
@@ -9,7 +9,7 @@ class OverrideServiceCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $definition = $container->getDefinition('sonata.admin.exporter');
-        $definition->setClass('Sigmapix\Sonata\ImportBundle\Export\Exporter');
+//        $definition = $container->getDefinition('sonata.admin.exporter');
+//        $definition->setClass('Sigmapix\Sonata\ImportBundle\Export\Exporter');
     }
 }
